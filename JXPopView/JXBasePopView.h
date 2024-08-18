73cedc74b0f36fd3c8b3ab64a38d12251c3ad37d
venus <venuslife@sina.cn>
2024-08-18 16:56:26 +0800

    ff

--- JXPopView/JXBasePopView.h
+++
@@ -1,7 +0,0 @@
-//
-//  MTNoticePopView.h
-//  TestDemo
-//
-//  Created by 金星 on 2020/9/4.
-//  Copyright © 2020 JG. All rights reserved.
-//
--- JXPopView/JXBasePopView.m
+++
@@ -1,9 +0,0 @@
-//
-//  MTNoticePopView.m
-//  TestDemo
-//
-//  Created by 金星 on 2020/9/4.
-//  Copyright © 2020 JG. All rights reserved.
-//
-
-
